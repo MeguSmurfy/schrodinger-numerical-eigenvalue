f591ca5e78e46ef82f3020f0969f1a7a11bd4093
Dinh-Quan Tran <74419286+MeguSmurfy@users.noreply.github.com>
2024-06-04 21:52:13 -0500

    Update scripts

--- revival.jl
+++ revival.jl
@@ -13,6 +13,7 @@ revivalSolReal = Solution.revivalSolReal
 
 xPoints = range(0, 2 * pi, step = 0.00005 * pi)
 
+#=
 print("Plotting real part over rational points: ")
 @time begin
     for timestep in rationalTimeSteps
@@ -31,22 +32,15 @@ print("Plotting real part over rational points: ")
         savefig(filename)
     end
 end
+=#
 
-print("Plotting real part difference over rational points: ")
-@time begin
-    for timestep in rationalTimeSteps
-        # make the plot
-        plot(xPoints, (@. solReal(xPoints, timestep * pi) - revivalSolReal(xPoints, timestep)), linecolor=:darkblue, linewidth=2)
+timestep = 0
 
-        # customization
-        plot!(grid=false, legend=false, size=(900, 600))
-
-        # save file
-        if timestep == 0
-            filename = "images\\$(Int.(c))_pi_stepfuncpi2_test\\rational_real_diff\\$(Int.(c))_pi_0_1000_00005pi_real.png"
-        else
-            filename = "images\\$(Int.(c))_pi_stepfuncpi2_test\\rational_real_diff\\$(Int.(c))_pi_$(numerator(timestep))pi$(denominator(timestep))_1000_00005pi_real.png"
-        end
-        savefig(filename)
-    end
+plot(xPoints, (@. solReal(xPoints, timestep * pi) - revivalSolReal(xPoints, timestep)), linecolor=:darkblue, linewidth=2)
+plot!(grid=false, legend=false, size=(900, 600))
+if timestep == 0
+    filename = "images\\$(Int.(c))_pi_stepfuncpi2_test\\rational_real_diff\\$(Int.(c))_pi_0_1000_00005pi_real.png"
+else
+    filename = "images\\$(Int.(c))_pi_stepfuncpi2_test\\rational_real_diff\\$(Int.(c))_pi_$(numerator(timestep))pi$(denominator(timestep))_1000_00005pi_real.png"
 end
+savefig(filename)
