b38a4584322f024c8f7c3b4c497564e2a97d348f
Dinh-Quan Tran <74419286+MeguSmurfy@users.noreply.github.com>
2024-08-12 11:44:39 -0500

    Testing new file structure

--- src/config/config.jl
+++ src/config/config.jl
@@ -7,15 +7,18 @@ increment = 0.0005
 mode = 0
 
 # revival mode
-revival = false
+revival = true
+
+# initial function designator
+initDesignator = "intdelta"
 
 # c is the step size of the potential function (integers)
 c = 9
 
 # p is the position of potential discontinuity
 # p_mode = 0 for rational, p_mode = 1 for irrational
-pMode = 1
-p = 2.01
+pMode = 0
+p = 1 // 2
 
 # timesteps for plotting
 rationalTimeSteps = [0, 1, 1 // 2, 1 // 3, 1 // 4, 1 // 5, 1 // 6, 1 // 7, 1 // 8, 1 // 9, 1 // 10, 2 // 15, 1 // 15, 1 // 30]
@@ -24,13 +27,10 @@ irrationalTimeSteps = [0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5]
 # initializers for negative eigenvalues
 numNegEigenvalues = 1000
 numNegEigenvaluesPicked = 4
-negEigenList = [1.77, 1.94, 3.18325, 3.1994]
+negEigenList = [1.59, 1.76, 3.02, 3.16]
 
 # initializers for negative eigenvalues
 numPosEigenvalues = 5
-posEigenList = [0.43, 1.66, 2.33, 2.71, 2.93]
-
-# initial function designator
-initDesignator = "intdelta"
+posEigenList = [1.1, 1.9, 2.45, 2.76, 2.94]
 
 end
